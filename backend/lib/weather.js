c60e636bf7e2c28e907e3fb2a3642bc9f9c33f6d
Anjali261 <mail2anjali.singh2610@gmail.com>
2022-07-23 15:55:03 +0530

    Adding Component and Creating API services.

--- backend/lib/weather.js
+++ backend/lib/weather.js
@@ -5,7 +5,9 @@ const env_config = config.get_active_config();
 exports.getweather = async(req,res)=>{
   let api_key=env_config.weatherAPI;
   let url = env_config.API_URL + "lat=35&lon=139" + "&appid=" + "api_key"  ;
+
   let data = await axios.get("url");
-  console.log(data);
-  return res.JSON(data)
+  let weather_info = data.data;
+
+  return res.status(200).send(weather_info);
 }
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app= express();
+const morgan= require("morgan");
+const cors = require('cors');
 const env_config = require("./backend/config/config")
 port = env_config.get_active_config().web_port;
+app.use(morgan('dev'));
 
 app.use(bodyParser,json({
   limit: '50mb',
@@ -11,10 +14,18 @@ app.use(bodyParser,json({
 }
 ));
 
+app.use((req,res,next) => {
+  res.header('Access-control-Allow-Origin' , '*');
+  next();
+});
+app.use(cors());
+app.use(express.json());
+
 let allrouter = require("./backend/routes/allrouter");
 const { json } = require("body-parser");
 app.use("/api",allrouter)
 
+;
 app.get("/",(req,res) =>
 {
     res.send({message:"hello, i am from backend"})
